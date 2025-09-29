a19120f8043169d053e0ea2f8546a336cc259b90
Jordan <joaojordan.shell@gmail.com>
2025-09-29 20:44:28 -0300

    feat: criado ponto de execução do algoritmo.

--- bubble-sort.cpp
+++ bubble-sort.cpp
@@ -8,39 +8,6 @@ void printArray(const std::vector<int>& arr) {
     std::cout << std::endl;
 }
 
-void bubbleSort(std::vector<int>& arr) {
-    int n = arr.size();
-
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                std::swap(arr[j], arr[j + 1]);
-            }
-        }
-    }
-}
-
-void bubbleSortOptimized(std::vector<int>& arr) {
-    int n = arr.size();
-    bool swapped;
-
-    for (int i = 0; i < n - 1; i++) {
-        swapped = false;
-
-        for (int j = 0; j < n - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                std::swap(arr[j], arr[j + 1]);
-                swapped = true;
-            }
-        }
-
-        if (!swapped) {
-            break;
-        }
-    }
-}
-
-// Versão com debug para mostrar as trocas
 void bubbleSortDebug(std::vector<int>& arr) {
     int n = arr.size();
     int comparisons = 0;
@@ -78,4 +45,13 @@ void bubbleSortDebug(std::vector<int>& arr) {
     printArray(arr);
     std::cout << "Total de comparações: " << comparisons << std::endl;
     std::cout << "Total de trocas: " << swaps << std::endl;
+}
+
+int main() {
+    std::vector<int> arr = {5, 2, 9, 1, 5, 6};
+
+    std::cout << "Executando:\n";
+    bubbleSortDebug(arr);
+
+    return 0;
 }
